4ddf6331a33831a98c2ec57cf467889905b7964a
stvnreis <stvnreis@gmail.com>
2023-11-14 17:50:55 -0300

    organização de pastas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import { Button } from '@nextui-org/react'
-import Link from 'next/link'
-import { Produto } from './components/Produto'
+import { Produto } from '../components/Produto'
 
 export default function Home() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Produto />
