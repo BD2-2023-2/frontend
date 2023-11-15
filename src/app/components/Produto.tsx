cffc74c7df5d5eb373306c50ea1d40b1bf13a49a
Steven Reis <stvnreis@gmail.com>
2023-11-15 20:42:49 -0300

    Produtos

--- src/app/components/Produto.tsx
+++ src/app/components/Produto.tsx
@@ -1,18 +1,28 @@
 import { Button, Card, CardBody, CardFooter } from "@nextui-org/react"
+import Image from "next/image"
 
-export const Produto = () => {
-  return <Card className="w-1/3">
+type ProdutoProps = {
+  id: number;
+  fotoUrl: string;
+  nome: string;
+  descricao: string;
+}
+
+export const Produto = ({id, fotoUrl, nome, descricao}: ProdutoProps) => {
+  return <Card className="w-[20rem]">
     <CardBody>
       <div className="flex w-full">
-        <div className="w-1/3">Foto do produto</div>
+        <div className="w-1/2 relative">
+          <Image src={fotoUrl} alt="url foto" style={{objectFit: 'cover', borderRadius: '10px'}} fill/>
+        </div>
         <div className="flex flex-col justify-center items-center w-full h-full">
-          <label>nome</label>
-          <span>descricao</span>
+          <label>{nome}</label>
+          <span>{descricao}</span>
+          <Button variant='shadow' color="primary" radius="none" size="sm" className="text-black mt-3">
+            Adicionar ao carrinho
+          </Button>
         </div>
       </div>
     </CardBody>
-    <CardFooter className="flex justify-center">
-      <Button variant='bordered' color="primary" radius="none" size="sm" className="text-black">Adicionar ao carrinho</Button>
-    </CardFooter>
   </Card>
 }
